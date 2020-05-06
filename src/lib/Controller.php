56d97cdc74020c9e3c806c66183569f8f8ab11fc
Loris <loris.ec@outlook.com>
2020-05-06 15:00:40 +0200

    add admin security check, this fix #8 and fix #9

--- src/lib/Controller.php
+++ src/lib/Controller.php
@@ -9,6 +9,15 @@ abstract class Controller
 
     public function __construct($manager)
     {
+        if (preg_match("/admin/i", $_SERVER['REQUEST_URI'])) {
+            $authenticator = new Authenticator();
+            $sessionInfo = $authenticator->getSessionInfo();
+
+            if ($sessionInfo['role'] != 'admin') {
+                $this->executeError(401);
+            }
+        }
+
         if (null != $manager) {
             $this->managers = new Managers('PDO', PDOFactory::getMysqlConnexion());
             $this->manager = $this->managers->getManagerOf($manager);
