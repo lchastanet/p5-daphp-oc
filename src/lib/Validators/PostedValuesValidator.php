d928a392660087913e6f16f4930390e17c3442e8
Loris <loris.ec@outlook.com>
2020-05-20 04:55:39 +0200

    security fix

--- src/lib/Validators/PostedValuesValidator.php
+++ src/lib/Validators/PostedValuesValidator.php
@@ -11,7 +11,7 @@ class PostedValuesValidator extends Validator
     public function isValid($val)
     {
         foreach ($val as $item) {
-            if (!isset($_POST[$item])) {
+            if (isset($_POST[$item])) {
                 $this->values[$item] = $_POST[$item];
             } else {
                 $this->setErrorMessage('La  valeur' . $item . 'est absente.');
