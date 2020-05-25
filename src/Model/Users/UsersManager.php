e3ce90eb6a08b8a6580aed1cb0b8c3d522f828f5
Loris <loris.ec@outlook.com>
2020-05-25 15:57:22 +0200

    add php doc to user manager abstract class

--- src/Model/Users/UsersManager.php
+++ src/Model/Users/UsersManager.php
@@ -20,12 +20,34 @@ abstract class UsersManager extends Manager
         }
     }
 
+    /**
+     * Méthode retournant un utilisateur ou null.
+     *
+     * @param $email string L'adresse email de l'utilisateur recherché.
+     *
+     * @return User L'utilisateur 
+     */
     abstract public function getByEmail($email);
 
+    /**
+     * Méthode renvoyant le nombre de news total.
+     *
+     * @return int
+     */
     abstract public function count();
 
+    /**
+     * Méthode retournant une liste de news demandée.
+     *
+     * @return array La liste des utilisateurs. Chaque entrée est une instance de User.
+     */
     abstract public function getAdminList();
 
+    /**
+     * Méthode validant le compte de l'utilisateur.
+     *
+     * @param $idUser int L'id de l'utilisateur.
+     */
     abstract public function validateAccount($idUser);
 
     /**
